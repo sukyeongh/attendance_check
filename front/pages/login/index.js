d6add27b85f89d8c74cff2c1bf1431f64d031ee3
sukyeongh <hsk10271@gmail.com>
2021-05-29 21:53:03 +0900

    Front) 기능 추가 : 교수/학생 로그인 및 로그아웃 기능 추가

--- front/pages/login/index.js
+++ front/pages/login/index.js
@@ -1,26 +1,113 @@
 import Router from 'next/router';
 import styles from '../../styles/Home.module.css';
-import { Button, TextField } from '@material-ui/core';
-import { styled } from '@material-ui/core/styles';
-import {} from '@material-ui/icons';
+import { Button, TextField, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+
+import axios from 'axios';
+import { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 const Login = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['userid']);
+  const [isStudent, setIsStudent] = useState(true);
+  const [user, setUser] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const [professor, setProfessor] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const onChangeUser = (e) => {
+    const { value, id } = e.target;
+    setUser({
+      ...user,
+      [id]: value,
+    });
+  };
+
+  const onChangeProfessor = (e) => {
+    const { value, id } = e.target;
+    setProfessor({
+      ...professor,
+      [id]: value,
+    });
+  };
+
+  const onLogin = async () => {
+    if (isStudent) {
+      const res = await axios.get(`http://localhost:3001/api/students/${user.id}`, {
+        params: {
+          pw: user.pw,
+        },
+      });
+
+      if (res.data.err) {
+        alert('로그인에 실패하였습니다.');
+      } else {
+        alert('로그인되었습니다.');
+        setCookie('userid', user.id, { maxAge: 2000 });
+        Router.push('/lectures');
+      }
+    } else {
+      const res = await axios.get(`http://localhost:3001/api/professors/${professor.id}`, {
+        params: {
+          pw: professor.pw,
+        },
+      });
+
+      if (res.data.err) {
+        alert('로그인에 실패하였습니다.');
+      } else {
+        alert('로그인되었습니다.');
+        Router.push('/professors');
+      }
+    }
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>로그인</h1>
 
+        <RadioGroup row aria-label='position' name='position' defaultValue='top'>
+          <FormControlLabel
+            value='end'
+            control={<Radio color='primary' checked={isStudent} />}
+            label='학생'
+            labelPlacement='start'
+            onClick={() => setIsStudent(true)}
+          />
+          <FormControlLabel
+            value='end'
+            control={<Radio color='primary' checked={!isStudent} />}
+            label='교수'
+            labelPlacement='start'
+            onClick={() => setIsStudent(false)}
+          />
+        </RadioGroup>
         <div className={styles.flexCenter} style={{ marginBottom: `20px` }}>
-          <TextField id='studentid' label='학번' variant='outlined' style={{ width: `300px`, margin: `5px` }} />
-          <TextField id='pw' label='비밀번호' variant='outlined' style={{ width: `300px`, margin: `5px` }} />
+          <TextField
+            id='id'
+            label={isStudent ? '학번' : '아이디'}
+            variant='outlined'
+            style={{ width: `300px`, margin: `5px` }}
+            value={isStudent ? user.id || '' : professor.id || ''}
+            onChange={isStudent ? onChangeUser : onChangeProfessor}
+          />
+          <TextField
+            id='pw'
+            label='비밀번호'
+            variant='outlined'
+            type='password'
+            style={{ width: `300px`, margin: `5px` }}
+            value={isStudent ? user.pw || '' : professor.pw || ''}
+            onChange={isStudent ? onChangeUser : onChangeProfessor}
+          />
         </div>
 
-        <Button
-          variant='outlined'
-          color='primary'
-          style={{ width: `300px`, margin: `5px` }}
-          onClick={() => Router.push('/lectures')}
-        >
+        <Button variant='outlined' color='primary' style={{ width: `300px`, margin: `5px` }} onClick={onLogin}>
           로그인
         </Button>
         <Button variant='outlined' style={{ width: `300px`, margin: `5px` }} onClick={() => Router.push('/')}>
--- front/pages/sign-up/index.js
+++ front/pages/sign-up/index.js
@@ -1,25 +1,136 @@
 import Router from 'next/router';
 import styles from '../../styles/Home.module.css';
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
 import { styled } from '@material-ui/core/styles';
 import {} from '@material-ui/icons';
 
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [isStudent, setIsStudent] = useState(true);
+  const [user, setUser] = useState({
+    id: '',
+    pw: '',
+    name: '',
+  });
+
+  const [professor, setProfessor] = useState({
+    id: '',
+    pw: '',
+    name: '',
+  });
+
+  const onChangeUser = (e) => {
+    const { value, id } = e.target;
+    setUser({
+      ...user,
+      [id]: value,
+    });
+  };
+
+  const onChangeProfessor = (e) => {
+    const { value, id } = e.target;
+    setProfessor({
+      ...professor,
+      [id]: value,
+    });
+  };
+
+  const onAddStudents = async () => {
+    const params = new URLSearchParams();
+    params.append('studentid', user.id);
+    params.append('pw', user.pw);
+    params.append('name', user.name);
+    const res = await axios.post(`http://localhost:3001/api/students`, params);
+
+    if (res.data) {
+      alert('회원가입되었습니다.');
+      setUser({
+        id: '',
+        pw: '',
+        name: '',
+      });
+    } else {
+      alert('회원가입에 실패하였습니다.');
+    }
+  };
+
+  const onAddProfessors = async () => {
+    const params = new URLSearchParams();
+    params.append('professorid', user.id);
+    params.append('pw', user.pw);
+    params.append('name', user.name);
+    const res = await axios.post(`http://localhost:3001/api/professors`, params);
+
+    if (res.data) {
+      alert('회원가입되었습니다.');
+      setProfessor({
+        id: '',
+        pw: '',
+        name: '',
+      });
+    } else {
+      alert('회원가입에 실패하였습니다.');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>회원가입</h1>
 
+        <RadioGroup row aria-label='position' name='position' defaultValue='top'>
+          <FormControlLabel
+            value='end'
+            control={<Radio color='primary' checked={isStudent} />}
+            label='학생'
+            labelPlacement='start'
+            onClick={() => setIsStudent(true)}
+          />
+          <FormControlLabel
+            value='end'
+            control={<Radio color='primary' checked={!isStudent} />}
+            label='교수'
+            labelPlacement='start'
+            onClick={() => setIsStudent(false)}
+          />
+        </RadioGroup>
+
         <div className={styles.flexCenter} style={{ marginBottom: `20px` }}>
-          <TextField id='studentid' label='학번' variant='outlined' style={{ width: `300px`, margin: `5px` }} />
-          <TextField id='pw' label='비밀번호' variant='outlined' style={{ width: `300px`, margin: `5px` }} />{' '}
-          <TextField id='name' label='이름' variant='outlined' style={{ width: `300px`, margin: `5px` }} />
+          <TextField
+            id='id'
+            label={isStudent ? '학번' : '아이디'}
+            variant='outlined'
+            style={{ width: `300px`, margin: `5px` }}
+            value={isStudent ? user.id : professor.id}
+            onChange={isStudent ? onChangeUser : onChangeProfessor}
+          />
+          <TextField
+            id='pw'
+            label='비밀번호'
+            type='password'
+            variant='outlined'
+            style={{ width: `300px`, margin: `5px` }}
+            value={isStudent ? user.pw : professor.pw}
+            onChange={isStudent ? onChangeUser : onChangeProfessor}
+          />
+          <TextField
+            id='name'
+            label='이름'
+            variant='outlined'
+            style={{ width: `300px`, margin: `5px` }}
+            value={isStudent ? user.name : professor.name}
+            onChange={isStudent ? onChangeUser : onChangeProfessor}
+          />
         </div>
 
-        <Button variant='outlined' color='primary' style={{ width: `300px`, margin: `5px` }}>
+        <Button
+          variant='outlined'
+          color='primary'
+          style={{ width: `300px`, margin: `5px` }}
+          onClick={isStudent ? onAddStudents : onAddProfessors}
+        >
           가입하기
         </Button>
         <Button
@@ -34,15 +145,5 @@ const SignUp = () => {
     </div>
   );
 };
-
-// SignUp.getInitialProps = async () => {
-//   const res = await axios.post('http://localhost:3001/students', {
-
-//   })
-
-//   return {
-//     user: res.data,
-//   };
-// };
 
 export default SignUp;
